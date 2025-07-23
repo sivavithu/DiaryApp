b0fb6f103888732e52a7d48915d7b9b5c9d4c18f
sivavithu <sivavithu15@live.com>
2025-07-23 22:08:07 +0530

    fetch and view

--- DiaryApp/Controllers/DiaryEntriesController.cs
+++ DiaryApp/Controllers/DiaryEntriesController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DiaryApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using DiaryApp.Models;
 
 namespace DiaryApp.Controllers
 {
     public class DiaryEntriesController : Controller
     {
+        private readonly ApplicationDbContext _db;
         public IActionResult Index()
         {
-            return View();
+            List<DiaryEntry> entries = _db.DiaryEntries.ToList();
+            return View(entries);
+        }
+        public DiaryEntriesController(ApplicationDbContext db)
+        {
+            _db = db;
         }
     }
 }
